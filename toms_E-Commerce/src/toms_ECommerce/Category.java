66e21ba7e3723357c74c379fd5c2a91d5c13c71b
bitbit138 <tombitran@gmail.com>
2025-04-28 17:32:38 +0300

    Add files via upload

---
+++ toms_E-Commerce/src/toms_ECommerce/Category.java
@@ -0,0 +1,9 @@
+package toms_ECommerce;
+
+// Represents different categories of products
+public enum Category {
+    KIDS,         // Category for kids' products
+    ELECTRICITY,  // Category for electrical products
+    OFFICE,       // Category for office products
+    CLOTHING      // Category for clothing products
+}
